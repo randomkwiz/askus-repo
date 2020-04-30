5a8f6fa757dd4e9be6348f1d9fc7e1562c3e852b
Ángela <angela.vazquez@iesnervion.es>
2020-04-30 20:42:50 +0200

    repository y use case

---
+++ AskUs/app/src/main/java/es/iesnervion/avazquez/askus/interfaces/AuthActivityInterface.kt
@@ -0,0 +1,6 @@
+package es.iesnervion.avazquez.askus.interfaces
+
+interface AuthActivityInterface {
+    fun goToSignUp()
+    fun goToLogIn()
+}
---
+++ AskUs/app/src/main/java/es/iesnervion/avazquez/askus/ui/usecase/SendAuthUseCase.kt
@@ -0,0 +1,43 @@
+package es.iesnervion.avazquez.askus.ui.usecase
+
+import es.iesnervion.avazquez.askus.models.Login
+import es.iesnervion.avazquez.askus.retrofit.interfaces.AuthInterface
+import es.iesnervion.avazquez.askus.ui.repositories.RepositoryInterface
+import es.iesnervion.avazquez.askus.utils.GlobalApplication
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import javax.inject.Inject
+
+class SendAuthUseCase {
+    @Inject
+    lateinit var requestInterface: AuthInterface
+
+    init {
+        GlobalApplication.applicationComponent?.inject(this)
+    }
+
+    fun getTokenLogin(repositoryInterface: RepositoryInterface, login: Login) {
+        val call = requestInterface.getTokenLogin(login)
+        repositoryInterface.onLoading(true)
+        call.enqueue(object : Callback<String> {
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                repositoryInterface.onLoading(false)
+                if(response.isSuccessful){
+                    response.body()?.replace("\"", "")?.toList()
+                        ?.let {
+                            repositoryInterface.onSuccess(it)
+                        }
+                }else{
+                    repositoryInterface.onSuccess(listOf(response.code()))
+                }
+
+            }
+
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                repositoryInterface.onLoading(false)
+                repositoryInterface.showError(true)
+            }
+        })
+    }
+}
