f563d518a1a304249b14ea0742c5c3eb061d384c
Ángela <angela.vazquez@iesnervion.es>
2020-05-23 16:28:24 +0200

    -código del mapper mejorado ahora con funcionalidad de kotlin y sin bucles

--- AskUs/app/src/main/java/es/iesnervion/avazquez/askus/mappers/Mapper.kt
+++ AskUs/app/src/main/java/es/iesnervion/avazquez/askus/mappers/Mapper.kt
@@ -1,23 +1,13 @@
 package es.iesnervion.avazquez.askus.mappers
 
 abstract class Mapper<D, M> {
-
     abstract fun dtoToModel(dto: D): M
-
     abstract fun modelToDto(obj: M): D
     fun dtoListToModelList(objsListDTO: List<D>): List<M> {
-        var objList = mutableListOf<M>()
-        for (i in objsListDTO.indices) {
-            dtoToModel(objsListDTO[i]).let { objList.add(it) }
-        }
-        return objList
+        return objsListDTO.map { dtoToModel(it) }
     }
 
     fun modelListToDtoList(objsList: List<M>): List<D> {
-        var objsDTOS = mutableListOf<D>()
-        for (i in objsDTOS.indices) {
-            modelToDto(objsList[i])?.let { objsDTOS.add(it) }
-        }
-        return objsDTOS
+        return objsList.map { modelToDto(it) }
     }
 }
